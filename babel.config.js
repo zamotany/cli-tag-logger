8e8c72585693eb11e09f5539ad05048883fb900c
zamotany <zamotany.oss@gmail.com>
2019-12-01 23:27:36 +0100

    feat: initial commit

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  presets: [
+    ['@babel/preset-env', { targets: { node: '10' }, modules: false }],
+    '@babel/preset-typescript',
+  ],
+  env: {
+    cjs: {
+      presets: [
+        ['@babel/preset-env', { targets: { node: '10' }, modules: 'cjs' }],
+      ],
+    },
+  },
+};
